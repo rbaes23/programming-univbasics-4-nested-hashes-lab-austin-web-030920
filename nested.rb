bee5d80babf47dc017155f633e4c5d5765dd0539
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-21 16:31:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,8 @@ def adding_matz
         }
      }
      
-  programer_hash[:yukihiro_matsumoto][]
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  return programmer_hash
     
 end
 
